a1250339c0ee336a34060edf9288719f56a5251c
Julian R <37474383+Jura17@users.noreply.github.com>
2018-05-03 14:08:40 +0200

    Update CarControls.cs

--- CarControls.cs
+++ CarControls.cs
@@ -4,123 +4,6 @@ using UnityEngine;
 
 public class CarControls : MonoBehaviour
 {
-    public WheelCollider FL_Col, FR_Col, BL_Col, BR_Col;
-    public float SteerForce, SteerForce2, MotorForce, BrakeForce;
-    public Rigidbody rb;
-    public float downForce = 111.81f;
-    public float topSpeed = 20f;
-    public float Strength = 10f;
-    public float SpeedBoostCount = 3;
-    public float BoostStrengh = 10f;
-    public float hangTimeSteerForce = 20;
-    Vector3 v3Force;
-    Vector3 v3SpeedBoost;
-    private bool isColliding = false;
-    public CollisionCheck colliderObj;
-
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-       
-    }
-
-    void FixedUpdate()
-    {
-        isColliding = colliderObj.getIsColliding();
-        Debug.Log(isColliding);
-        if (isColliding) {
-            //car is in contact with racing track
-
-            float horizontal = Input.GetAxis("Horizontal") * SteerForce;
-            float horizontal2 = Input.GetAxis("Horizontal") * SteerForce2;
-            FL_Col.steerAngle = horizontal;
-            FR_Col.steerAngle = horizontal;
-            BL_Col.steerAngle = -horizontal2;
-            BR_Col.steerAngle = -horizontal2;
-
-
-            //forward thrust
-            v3Force = Strength * transform.forward * Time.deltaTime;
-            v3SpeedBoost = Strength * 2 * transform.forward * Time.deltaTime;
-            if (Input.GetKey("w"))
-            {
-                rb.AddForce(v3Force);
-                rb.velocity = Vector3.ClampMagnitude(rb.velocity, topSpeed);
-            }
-
-            if (Input.GetKey("s"))
-            {
-                rb.AddForce(-v3Force);
-                rb.velocity = Vector3.ClampMagnitude(rb.velocity, topSpeed);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space) && Strength == 5000000)
-            {
-                if (SpeedBoostCount > 0)
-                {
-                    Strength = Strength * BoostStrengh;
-                    rb.AddForce(v3Force);
-
-                    SpeedBoostCount--;
-                }
-                StartCoroutine("CallAfterTime");
-                print(Strength);
-            }
-
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                SteerForce2 = 60f;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftShift))
-            {
-                SteerForce2 = 0f;
-            }
-        }
-        else {
-        //car is in air
-            v3Force = Strength * transform.forward * Time.deltaTime;
-            v3SpeedBoost = Strength * 2 * transform.forward * Time.deltaTime;
-            if (Input.GetKey("w"))
-            {
-                rb.AddForce(v3Force);
-                rb.velocity = Vector3.ClampMagnitude(rb.velocity, topSpeed);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space) && Strength == 5000000)
-            {
-                if (SpeedBoostCount > 0)
-                {
-                    Strength = Strength * BoostStrengh;
-                    rb.AddForce(v3Force);
-
-                    SpeedBoostCount--;
-                }
-                StartCoroutine("CallAfterTime");
-                print(Strength);
-            }
-            if(Input.GetKey("d"))
-            transform.Rotate(0, hangTimeSteerForce * Time.deltaTime, 0);
-
-            if (Input.GetKey("a"))
-            transform.Rotate(0, -hangTimeSteerForce * Time.deltaTime, 0);
-
-        }
-    }
-
-
-    IEnumerator CallAfterTime()
-    {
-        yield return new WaitForSeconds(3);
-        Strength = Strength / BoostStrengh;
-    }
-}
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CarControls : MonoBehaviour
-{
 	public WheelCollider FL_Col, FR_Col, BL_Col, BR_Col;
     public float SteerForce;
 	public float topSpeed = 20f;
@@ -130,49 +13,28 @@ public class CarControls : MonoBehaviour
 	public float hangTimeSteerForce = 20;
     public float downForce = 0f;
 	public CollisionCheck colliderObj;
+    public float speedReducerStrength;
 
 	private Rigidbody car_rb;
     private float DriftForce;
     private float velocityMagnitude = 0;
 	private bool isColliding = true;
-	Vector3 v3Force;
-	Vector3 v3SpeedBoost;
+    private float initialLinearDrag;
+	private Vector3 v3Force;
+	private Vector3 v3SpeedBoost;
 
 
 	void Start()
 	{
 		car_rb = GetComponent<Rigidbody>();
+        initialLinearDrag = car_rb.drag;
 	}
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("RacingTrack"))
-        {
-            //Debug.Log("Car rb is colliding");
-        }
-    }
-
-    public void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("RacingTrack"))
-        {
-            //Debug.Log("Car rb is still mucho mucho");
-        }
-    }
-
-    public void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("RacingTrack"))
-        {
-            //Debug.Log("Car rb is NOT colliding");
-        }
-    }
 
     void FixedUpdate()
 	{
         AddDownForce();
 
         isColliding = colliderObj.getIsColliding();
-		//Debug.Log(isColliding);
 		if (isColliding)
 		{
             //car is in contact with racing track
@@ -199,11 +61,9 @@ public class CarControls : MonoBehaviour
 				car_rb.AddForce(-v3Force);
 				car_rb.velocity = Vector3.ClampMagnitude(car_rb.velocity, topSpeed);
 			}
-
             
-
-            if (Input.GetKeyUp(KeyCode.Space) && Strength == 5000000)
-			{
+            if (Input.GetKeyUp(KeyCode.Space) && Strength == 8500000)
+            {
 				if (SpeedBoostCount > 0)
 				{
 					Strength = Strength * BoostStrengh;
@@ -212,7 +72,6 @@ public class CarControls : MonoBehaviour
 					SpeedBoostCount--;
 				}
 				StartCoroutine("CallAfterTime");
-				print(Strength);
 			}
 
 			if (Input.GetKeyDown(KeyCode.LeftShift))
@@ -235,8 +94,9 @@ public class CarControls : MonoBehaviour
 				car_rb.velocity = Vector3.ClampMagnitude(car_rb.velocity, topSpeed);
 			}
 
-			if (Input.GetKeyUp(KeyCode.Space) && Strength == 5000000)
-			{
+            
+            if (Input.GetKeyUp(KeyCode.Space) && Strength == 8500000)
+            {
 				if (SpeedBoostCount > 0)
 				{
 					Strength = Strength * BoostStrengh;
@@ -245,7 +105,6 @@ public class CarControls : MonoBehaviour
 					SpeedBoostCount--;
 				}
 				StartCoroutine("CallAfterTime");
-				print(Strength);
 			}
 			if (Input.GetKey("d"))
 				transform.Rotate(0, hangTimeSteerForce * Time.deltaTime, 0);
@@ -258,6 +117,24 @@ public class CarControls : MonoBehaviour
     private void AddDownForce()
     {
         car_rb.AddRelativeForce(-transform.up * downForce * velocityMagnitude);
+    }
+
+    //------SPEED REDUCER-------
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("SpeedReducer"))
+        {
+            car_rb.drag = speedReducerStrength;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("SpeedReducer"))
+        {
+            car_rb.drag = initialLinearDrag;
+        }   
     }
 
 	IEnumerator CallAfterTime()
